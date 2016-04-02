9bc4748d50c572d308ed78f90682180695018be8
R.Fujimoto <fujimoto.ryunosuke@gmail.com>
2016-04-02 20:33:10 +0900

    remove heroku folder

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/heroku'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
